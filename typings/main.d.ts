7542094dcdc2a68721460ec6d28e5c217c7cbbcf
Christian Cueni <christian.cueni@gmail.com>
2016-03-15 14:11:27 +0100

    updates typings.json

--- typings/main.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="main/ambient/angular-mocks/index.d.ts" />
-/// <reference path="main/ambient/angular-resource/index.d.ts" />
-/// <reference path="main/ambient/angular-route/index.d.ts" />
-/// <reference path="main/ambient/angular/index.d.ts" />
-/// <reference path="main/ambient/jasmine/index.d.ts" />
-/// <reference path="main/ambient/jquery/index.d.ts" />
-/// <reference path="main/ambient/lodash/index.d.ts" />
-///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
