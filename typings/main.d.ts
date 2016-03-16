3736eae6f5a44cd0629c8ef93ddb49992b4a42c1
Christian Cueni <christian.cueni@gmail.com>
2016-03-16 07:58:45 +0100

    adds ts definitions

---
+++ typings/main.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="main/ambient/angular-mocks/index.d.ts" />
+/// <reference path="main/ambient/angular-resource/index.d.ts" />
+/// <reference path="main/ambient/angular-route/index.d.ts" />
+/// <reference path="main/ambient/angular/index.d.ts" />
+/// <reference path="main/ambient/jasmine/index.d.ts" />
+/// <reference path="main/ambient/jquery/index.d.ts" />
+/// <reference path="main/ambient/lodash/index.d.ts" />
